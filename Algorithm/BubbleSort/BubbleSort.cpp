118bf2e14d68ee03f014255fcf6851ade7f28330
GCMonster <1724264854@qq.com>
2020-05-26 22:27:58 +0800

    BubbleSort&InsertSort

---
+++ Algorithm/BubbleSort/BubbleSort.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+void show(int[]);
+
+int main()
+{
+	int nums[] =  {9, 6, 2, 7, 1, 8, 6, 3, 0};
+	for (int i = 0; i < 9; ++i)
+	{
+		for (int j = i + 1; j < 9; ++j)
+		{
+			if (nums[i] > nums[j])
+			{
+				int tmp = nums[i];
+				nums[i] = nums[j];
+				nums[j] = tmp;
+			}
+		}
+	}
+	show(nums);
+	system("pause");
+}
+
+void show(int nums[])
+{
+	for (int i = 0; i < 9; ++i)
+	{
+		cout << nums[i] << '\t';
+	}
+}
---
+++ Algorithm/InsertSort/InsertSort.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+void swap(int[], int, int);
+void show(int[]);
+
+int main()
+{
+	int nums[10], num = 0;
+	while (num < 10)
+	{
+		int tmp;
+		cin >> tmp;
+		nums[num] = tmp;
+		++num;
+	}
+
+	// 选择排序
+	for (int i = 0; i < 9; ++i)
+	{
+		int tmp = i;
+		for (int j = i + 1; j < 10; ++j)
+		{
+			if (nums[j] < nums[tmp])
+			{
+				tmp = j;
+			}
+		}
+		swap(nums, i, tmp);
+	}
+
+	show(nums);
+	system("pause");
+}
+void swap(int nums[], int x, int y)
+{
+	int tmp = nums[x];
+	nums[x] = nums[y];
+	nums[y] = tmp;
+}
+void show(int nums[])
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << nums[i] << '\t';
+	}
+}
